5487d4ad05f43c2be28e789d5b1655f7b6d14b05
Prateek Varma <prtk.varma@gmail.com>
2022-07-19 16:44:42 -0600

    Added some filter buttons, and wrote basic tests for them

--- src/components/Header.test.js
+++ src/components/Header.test.js
@@ -13,9 +13,18 @@ it("should show logo", () => {
 });
 
 it("should show search", () => {
-    expect(getByTestId(container, "search")).toBeTruthy();
-  });
+  expect(getByTestId(container, "search")).toBeTruthy();
+});
 
-  it("should show the menu", () => {
-    expect(getByTestId(container, "menu")).toBeTruthy();
-  });
+it("should show the menu", () => {
+  expect(getByTestId(container, "menu")).toBeTruthy();
+});
+
+it("should show the search filters", () => {
+  expect(getByTestId(container, "home-type")).toBeTruthy();
+  expect(getByTestId(container, "dates")).toBeTruthy();
+  expect(getByTestId(container, "guests")).toBeTruthy();
+  expect(getByTestId(container, "price")).toBeTruthy();
+  expect(getByTestId(container, "rooms")).toBeTruthy();
+  expect(getByTestId(container, "amenities")).toBeTruthy();
+});
